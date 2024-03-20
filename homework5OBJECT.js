e5f671472ba1d4800609c7d7f26fea1a22f31378
Tetiana Kulyk <126335301+Tetiana-Kulyk@users.noreply.github.com>
2024-03-20 22:20:58 +0200

    Update homework5OBJECT.js

--- homework5OBJECT.js
+++ homework5OBJECT.js
@@ -2,17 +2,15 @@ var pet = {
     type: "Dog",
     name: "Oliver",
     breed: "Yorkshire terrier", 
-    weight: 2
-}
-
-getInfo = function(){
-    for (let key in pet){
-        console.log(`${key}: ${pet[key]}`)
+    weight: 2,
+    getInfo: function() {
+        for (let key in this) {
+            console.log(`${key}: ${this[key]}`);
+        }
     }
-}
+};
 
-console.log(pet.getInfo())
+pet.getInfo();
 
-pet.color = "Silver"
-
-//console.log(pet.getInfo())
+pet.color = "Silver";
+pet.getInfo(); 
