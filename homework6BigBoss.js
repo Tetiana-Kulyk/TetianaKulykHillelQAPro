5769156a5c99c7a9004d64bb2fd4b9ae354d2dd4
Tetiana Kulyk <126335301+Tetiana-Kulyk@users.noreply.github.com>
2024-04-01 23:09:53 +0300

    Add files via upload

---
+++ homework6BigBoss.js
@@ -0,0 +1,58 @@
+const company = {
+    name: 'Велика Компанія',
+    type:'Головна компанія',
+    platform: 'Платформа для продажу квитків',
+    sellsSolution: 'Рішення для продажу квитків',
+    clients: [
+        {
+            name: 'Клієнт 1',
+            type: 'subCompany',
+            uses: 'ПО для продажу квитків',
+            sells: 'Рішення для продажу квитків',
+            partners: [
+                {
+                    name: 'Клієнт 1.1',
+                    type: 'subSubCompany',
+                    uses: 'Рішення для продажу квитків',
+                    sells: 'Рішення для продажу квитків',
+                },
+                {
+                    name: 'Клієнт 1.2',
+                    type: 'subSubCompany',
+                    uses: 'Рішення для продажу квитків',
+                    sells: 'Рішення для продажу квитків',
+                    partners: [
+                        {
+                            name: 'Клієнт 1.2.3',
+                            type: 'subSubCompany',
+                            uses: 'Рішення для продажу квитків',
+                            sells: 'Рішення для продажу квитків',
+                        }
+                    ]
+                }
+            ]
+        },
+        {
+            name: 'Клієнт 2',
+            type: 'subCompany',
+            uses: 'ПО для продажу квитків',
+            sells: 'Рішення для продажу квитків'
+        }
+    ],
+    findValueByKey: function(companyName, clients = this.clients) {
+        for (let client of clients) {
+            if (client.name === companyName) {
+                return client;
+            }
+            if (client.partners) {
+                const result = this.findValueByKey(companyName, client.partners);
+                if (result) {
+                    return result;
+                }
+            }
+        }
+        return null;
+    }
+};
+
+console.log(company.findValueByKey('Клієнт 1.2.3'));
